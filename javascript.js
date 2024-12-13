dc1cfc7508433e3944f11ba414306518600ab380
tlacuacheDev <187345755+tlacuacheDev@users.noreply.github.com>
2024-12-12 23:02:11 -0600

    Create getHumanChoice FX

--- javascript.js
+++ javascript.js
@@ -1,3 +1,14 @@
 function getComputerChoice() {
   return Math.floor(Math.random() * 3);
 }
+
+function getHumanChoice() {
+  let humanChoice = prompt("What's your sign?");
+  humanChoice = humanChoice.toLocaleLowerCase()
+  if (humanChoice === 'scissors' || humanChoice === 'rock' || humanChoice === 'paper') {
+    return humanChoice
+  }
+  else{
+    alert("Invalid Input.")
+  }
+}
