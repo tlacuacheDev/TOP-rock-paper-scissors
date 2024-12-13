9391eeb70e2953a40843b983d5f54d23d0246a2d
tlacuacheDev <187345755+tlacuacheDev@users.noreply.github.com>
2024-12-13 00:30:41 -0600

    Create playRound Fx

--- javascript.js
+++ javascript.js
@@ -1,14 +1,58 @@
 function getComputerChoice() {
-  return Math.floor(Math.random() * 3);
+  computerChoice = Math.floor(Math.random() * 3);
+  switch(computerChoice){
+    case 1:
+      return 'scissors'
+    case 2:
+      return 'paper'
+    case 3:
+      return 'rock'
+    default:
+      console.log("Invalid Input.")
+      return "Invalid Input."
+
+  }  
 }
 
 function getHumanChoice() {
-  let humanChoice = prompt("What's your sign?");
+  let humanChoice = prompt("Choose between Rock, Paper or Scissors");
   humanChoice = humanChoice.toLocaleLowerCase()
   if (humanChoice === 'scissors' || humanChoice === 'rock' || humanChoice === 'paper') {
     return humanChoice
   }
   else{
-    alert("Invalid Input.")
+    console.log("Invalid Input.")
+    return "Invalid Input."
   }
 }
+
+function playRound(humanChoice, computerChoice){
+  
+  if (humanChoice === computerChoice){
+    
+  }
+  else if( humanChoice === "Invalid Input." || computerChoice === "Invalid Input."){
+
+  }
+  else {
+  
+    switch (humanChoice) {
+      case 'scissors':
+        computerChoice === "paper" ? humanScore++ : computerScore ++
+        break;
+      case 'rock':
+        computerChoice === "scissors" ? humanScore++ : computerScore ++
+        break;
+      case 'paper':
+        computerChoice === "rock" ? humanScore++ : computerScore ++
+      break;
+  }
+
+  }
+  
+  
+  
+}
+
+let humanScore = 0;
+let computerScore = 0;
